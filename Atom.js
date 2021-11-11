8e8357a030119d7ca604ee8acc0b8396b52f3dc0
Tarun/Theo Weisz <theo.weisz@gmail.com>
2021-11-11 18:55:17 +0000

    first commit

---
+++ Atom.js
@@ -0,0 +1,44 @@
+class Atom {
+  #secret_name;
+  #inbox;
+  
+	constructor(name) {
+  	this.name = name;
+    this.#secret_name = name;
+    this.#inbox = [];
+  }
+  
+  getSecretName() {
+  	return this.#secret_name;
+  }
+  
+  addToInbox(message) {
+  	this.#inbox.push(message);
+  }
+  
+  processInbox() {
+		let message = this.#inbox.shift();
+    console.log(this.#inbox);
+    if (message.req == "change secret_name" && typeof(message.name)==='string') {
+    	this.#secret_name = message.name;
+    }
+  }
+  
+}
+
+// let myAtom = new Atom("Tarun_Atom");
+
+// console.log(myAtom.getSecretName());
+ 
+// myAtom.addToInbox({src:'global', req:"change _secret_name", name:"Tarun2"});
+// myAtom.addToInbox({src:'global', req:"change secret_name", name:["Tarun2"]});
+// myAtom.addToInbox({src:'global', req:"change secret_name", name:"Tarun2"});
+
+// myAtom.processInbox();
+// console.log(myAtom.getSecretName());
+
+// myAtom.processInbox();
+// console.log(myAtom.getSecretName());
+
+// myAtom.processInbox();
+// console.log(myAtom.getSecretName());
